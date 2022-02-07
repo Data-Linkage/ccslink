713aaa1597df2c4e83e563ef9c3d0843030614e2
Anthony Edwards <51706026+anthonye93@users.noreply.github.com>
2022-02-07 10:05:21 +0000

    Update D_Reloads.py

--- pipeline_scripts/people_probabilistic/D_Reloads.py
+++ pipeline_scripts/people_probabilistic/D_Reloads.py
@@ -21,20 +21,8 @@ exec(open("/home/cdsw/collaborative_method_matching/CCSLink/Packages.py").read()
 cms_decisions = sparkSession.createDataFrame([], StructType([StructField("id_ccs", StringType(), True),
                                                              StructField("id_cen", StringType(), True)]))
 
-# File paths of all person decisions made in the CMS (could include matches made in the HH journey too)
-paths = ['/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/08/16/16/residents_match/*.csv',         # All CRZ matches after resolving conflicts
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/08/16/17/residents_match/*.csv',         # Extra CRZ matches after resident name fix
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/08/27/30/householdsppl_match/ab/*.csv',  # Households AB person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/09/02/37/householdsppl_match/cd/*.csv',  # Households CD (1) person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/09/02/38/householdsppl_match/cd/*.csv',  # Households CD (2) person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/10/27/113/residents_match/*.csv',        # V16: All CRZ matches after resolving conflicts
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/11/02/123/householdsppl_match/ab/*.csv', # V16: Households AB person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/11/08/125/householdsppl_match/cd/*.csv', # V16: Households CD person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/11/17/134/residents_match/*.csv',        # V20: All CRZ matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/11/22/139/householdsppl_match/ab/*.csv', # V20: Households AB person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/11/23/140/householdsppl_match/cd/*.csv', # V20: Households CD person matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/12/08/155/residents_match/*.csv',        # V20: Presearch matches
-         '/data/dap/c21_processing_zone/c21_cmatch_hdfs_h/file/cms_outputs/2021/12/09/159/householdsppl_match/cd/*.csv'] # V20: Households EF person matches
+# File paths of all person decisions made during clerical matching (could include matches made in the HH journey too)
+paths = []
 
 # Run if paths list not empty
 if len(paths) > 0:
